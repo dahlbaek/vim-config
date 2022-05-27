fa947f108b435474a4975e822cc1ec8386de6249
dahlbaek <30782351+dahlbaek@users.noreply.github.com>
2022-05-27 19:36:00 +0200

    cleanup cwindow

--- svim/nvim/init.lua
+++ svim/nvim/init.lua
@@ -9,7 +9,7 @@ vim.o.grepprg = "rg --vimgrep --smart-case"
 ----------------------------------
 -- Commands ----------------------
 ----------------------------------
-vim.api.nvim_create_user_command('Rg', 'silent grep! <args> | copen | cwindow', { nargs = "+" })
+vim.api.nvim_create_user_command('Rg', 'silent grep! <args> | copen', { nargs = "+" })
 vim.api.nvim_create_user_command('Metals', 'lua vim.lsp.buf.execute_command({ command = "metals.<args>" })', { nargs = 1 })
 
 ----------------------------------
@@ -18,7 +18,7 @@ vim.api.nvim_create_user_command('Metals', 'lua vim.lsp.buf.execute_command({ co
 vim.g.mapleader = " "
 local opts = { noremap = true }
 api.nvim_set_keymap("i", "<C-Space>", "<C-X><C-O>", opts)
-api.nvim_set_keymap("n", "<leader>d", ":lua vim.diagnostic.setqflist()<CR>:cwindow<CR>", opts)
+api.nvim_set_keymap("n", "<leader>d", "<cmd>lua vim.diagnostic.setqflist()<CR>", opts)
 api.nvim_set_keymap("n", "<C-j>", "<cmd>cnext<CR>zz", opts)
 api.nvim_set_keymap("n", "<C-k>", "<cmd>cprevious<CR>zz", opts)
 
@@ -36,8 +36,8 @@ local on_attach = function(client, bufnr)
   -- LSP mappings
   map("n", "gD", "<cmd>lua vim.lsp.buf.definition()<CR>")
   map("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>")
-  map("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>:cwindow<CR>")
-  map("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>:cwindow<CR>")
+  map("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>")
+  map("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>")
   map("n", "gds", "<cmd>lua vim.lsp.buf.document_symbol()<CR>")
   map("n", "gws", "<cmd>lua vim.lsp.buf.workspace_symbol()<CR>")
   map("i", "<C-h>", "<cmd>lua vim.lsp.buf.signature_help()<CR>")
