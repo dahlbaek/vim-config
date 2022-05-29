af262608f2419f5b34001d7f972716a04eece305
dahlbaek <30782351+dahlbaek@users.noreply.github.com>
2022-05-29 21:48:51 +0200

    Use metals status bar extension

--- svim/nvim/init.lua
+++ svim/nvim/init.lua
@@ -1,3 +1,29 @@
+-- inspired by how nvim-metals handles the metals status bar extension
+-- https://github.com/scalameta/nvim-metals/blob/9f8272802d35928df6c739f8e06f0e2767ad53a7/lua/metals/status.lua
+local function set_status(status)
+  vim.api.nvim_set_var("metals_status", status)
+end
+
+local function handle_status(_, status, _)
+  if status.hide then
+    set_status("")
+  elseif status.text then
+    set_status(status.text)
+  end
+end
+
+function CustomStatusline()
+  status = vim.g["metals_status"] or ""
+  return table.concat({
+    "%<",
+    status,
+    "%=",
+    "%t"
+  })
+end
+
+vim.opt.statusline = "%!luaeval('CustomStatusline()')"
+
 local on_attach = require("base-config").on_attach
 
 local root_dir = function()
@@ -5,6 +31,16 @@ local root_dir = function()
 end
 
 require("lspconfig").metals.setup({
+  handlers = {
+    ["metals/status"] = handle_status
+  },
+  init_options = {
+    statusBarProvider = 'on',
+    isHttpEnabled = true,
+    compilerOptions = {
+      snippetAutoIndent = false,
+    },
+  },
   on_attach = on_attach,
   root_dir = root_dir,
 })
